2a1685ef1f482ecbbce012f7d0b7d8f458bb4391
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-07-04 03:25:00 +0530

    fixed typo

--- components/jobdetails/about/About.jsx
+++ components/jobdetails/about/About.jsx
@@ -6,7 +6,7 @@ import styles from './about.style'
 const About = ({ info }) => {
   return (
     <View style={styles.container}>
-      <Text style={styles.headText}>About the hob:</Text>
+      <Text style={styles.headText}>About the Job:</Text>
       <View style={styles.contentBox}>
         <Text style={styles.contextText}>{info}</Text>
       </View>
